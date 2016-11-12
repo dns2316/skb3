3dd82fc81026874104259dab15d9182d7d8c6ef7
dns2316 <dns2316@gmail.com>
2016-11-12 21:56:43 +0200

    'start'

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import canonize from './canonize';
+
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => {
+  res.json({
+    hello: 'JS World',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Your app listening on port 3000!');
+});

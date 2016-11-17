8d065f11654b3946c186e227f855fd517ce1b480
dns2316 <dns2316@gmail.com>
2016-11-17 12:49:33 +0200

    help change to module style from node.js

--- src/pc.js
+++ src/pc.js
@@ -2,23 +2,34 @@ import fetch from 'node-fetch';
 
 const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
 let pc = {};
-export default () => fetch(pcUrl)
-    .then(async (res) => {
-      pc = await res.json();
-      return pc;
-      console.log('pc ', pc);
-    })
-    .catch (err => {
-      console.log('Error: ', err);
-    });
-/*
-const url = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
+// export default () => fetch(pcUrl)
+//     .then(async (res) => {
+//       pc = await res.json();
+//       return pc;
+//       console.log('pc ', pc);
+//     })
+//     .catch (err => {
+//       console.log('Error: ', err);
+//     });
 
-let cachedResult;
+// Vladimir Starkov
+// const url = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
+//
+// let cachedResult;
+//
+// const addToCache = _ => { cachedResult = _; return _; }
+//
+// export default () => cachedResult
+//   ? Promise.resolve(cachedResult)
+//   : fetch(url).then(res => res.json()).then(addToCache);
+// end Vladimir Starkov
 
-const addToCache = _ => { cachedResult = _; return _; }
-
-export default () => cachedResult
-  ? Promise.resolve(cachedResult)
-  : fetch(url).then(res => res.json()).then(addToCache);
-*/
+// Сергей Сова
+let cachePc = null;
+export default () =>
+  cachePc
+  ? Promise.resolve(cachePc)
+  : fetch(pcUrl)
+    .then(res => res.json())
+    .then(pc => cachePc = pc)
+// end Сергей Сова
--- src/volumes.js
+++ src/volumes.js
@@ -1,8 +1,9 @@
 import notFound from './notFound';
 import pc from './pc';
 
-export default (req, res) => {
-  let pc = pc();
+export default async (req, res) => {
+  let pc = await pc();
+
   console.log('start volumes', pc);
   console.log('start volumes', pc.hdd);
   // "тупо проходим по всем его элементам, и в новый массив суммируем по следующей логике sums[pc.hdd[x].volume] += pc.hdd[x].size"

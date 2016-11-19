4b712ce4059f948cdeb5973fa981c4bd8cffd0b0
dns2316 <dns2316@gmail.com>
2016-11-19 15:24:59 +0200

    style modules completed. Code = 1!

--- src/main3a.js
+++ src/main3a.js
@@ -1,22 +1,47 @@
 import notFound from './notFound';
 import requestPc from './pc';
 
+const toType = obj =>
+  ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+
 export default async function main3a(req, res) {
   try {
     const pc = await requestPc();
     console.log('pc in main3a: ', pc);
 
-    const toType = obj =>
-      ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
-
     console.log(req.url);
     console.log(req.url.split('/'));
 
-    const parts = req.url.split('/')
-      .filter(el => el).slice(1)
-      .reduce((prev, curr) => {
+    const parts = req.url.split('/') // Делит строку на массив делителем '/'
+      .filter(el => el).slice(1) // Берет элементы с индексом 1+ (0 индекс = 3а)
+      .reduce((prev, curr) => { // Перебирает массив prev = 0 индекс, curr = 1 индекс. Прерыдущий индекс и тот который в итерации.
         if (toType(prev) === 'object') {
-          if ({}.hasOwnProperty.call(prev, curr)) {
+          /*
+          Метод obj.hasOwnProperty(prop) возвращает логическое значение, указывающее, содержит ли объект указанное свойство.
+
+          prop
+            Имя проверяемого свойства.
+
+          Каждый объект, произошедший от Object, наследует метод hasOwnProperty. Этот метод может использоваться для определения того,
+          содержит ли объект указанное свойство в качестве собственного свойства объекта; в отличие от оператора in, этот метод не проверяет существование свойств
+          в цепочке прототипов объекта.
+
+          var foo = {
+              hasOwnProperty: function() {
+                return false;
+              },
+              bar: 'Тут драконы'
+          };
+
+          foo.hasOwnProperty('bar'); // всегда возвращает false
+
+          Используем метод hasOwnProperty другого объекта и вызываем его с передачей foo в качестве this
+          ({}).hasOwnProperty.call(foo, 'bar'); // true
+
+          Также для этих целей можно использовать свойство hasOwnProperty из прототипа Object
+          Object.prototype.hasOwnProperty.call(foo, 'bar'); // true
+          */
+          if ({}.hasOwnProperty.call(prev, curr)) { // Но почему бы не использовать toType?
             return prev[curr];
           }
         } else if (toType(prev) === 'array') {
@@ -26,8 +51,8 @@ export default async function main3a(req, res) {
         }
           return undefined;
       }, pc);
-    console.log(pc);
-    console.log(parts);
+    console.log('pc in main3a: ', pc);
+    console.log('parts in main3a: ', parts);
     if (parts === undefined) {
       notFound(res);
     }
